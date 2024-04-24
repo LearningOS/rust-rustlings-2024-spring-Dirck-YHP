ccca5d65de78bbc8adbd34e87a19b420e310dd7e
Dirck-YHP <1941803238@qq.com>
2024-04-24 13:19:32 +0800

    update

--- exercises/smart_pointers/cow1.rs
+++ exercises/smart_pointers/cow1.rs
@@ -12,11 +12,11 @@
 //
 // Execute `rustlings hint cow1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 use std::borrow::Cow;
 
 fn abs_all<'a, 'b>(input: &'a mut Cow<'b, [i32]>) -> &'a mut Cow<'b, [i32]> {
+    let mut data = input.to_mut();
     for i in 0..input.len() {
         let v = input[i];
         if v < 0 {
@@ -48,7 +48,8 @@ mod tests {
         let slice = [0, 1, 2];
         let mut input = Cow::from(&slice[..]);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 
@@ -60,7 +61,8 @@ mod tests {
         let slice = vec![0, 1, 2];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 
@@ -72,7 +74,8 @@ mod tests {
         let slice = vec![-1, 0, 1];
         let mut input = Cow::from(slice);
         match abs_all(&mut input) {
-            // TODO
+            Cow::Owned(_) => Ok(()),
+            _ => Err("Expected owned value"),
         }
     }
 }
--- exercises/threads/threads2.rs
+++ exercises/threads/threads2.rs
@@ -7,33 +7,65 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-use std::sync::Arc;
+// use std::sync::{Arc, Mutex};
+// use std::thread;
+// use std::time::Duration;
+
+// struct JobStatus {
+//     jobs_completed: Mutex<u32>,
+// }
+
+// fn main() {
+//     let status = Arc::new(JobStatus { jobs_completed: Mutex::new(0) });
+//     let mut handles = vec![];
+//     for _ in 0..10 {
+//         let status_shared = Arc::clone(&status);
+//         let handle = thread::spawn(move || {
+//             thread::sleep(Duration::from_millis(250));
+//             // TODO: You must take an action before you update a shared value
+//             let mut status_shared_ = status_shared.jobs_completed.lock().unwrap();
+//             *status_shared_ += 1;
+//         });
+//         handles.push(handle);
+//     }
+//     for handle in handles {
+//         handle.join().unwrap();
+//         // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
+//         // anything interesting in the output? Do you have to 'join' on all the
+//         // handles?
+//         let jobs_com = status.jobs_completed.lock().unwrap();
+//         println!("jobs completed {}", *jobs_com);
+//     }
+// }
+
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 struct JobStatus {
-    jobs_completed: u32,
+    jobs_completed: Mutex<u32>,
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(JobStatus { jobs_completed: Mutex::new(0) });
     let mut handles = vec![];
-    for _ in 0..10 {
+    for i in 1..=10 {
         let status_shared = Arc::clone(&status);
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
-            // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            // 加锁以获取对共享状态的独占访问权
+            let mut status_shared_ = status_shared.jobs_completed.lock().unwrap();
+            *status_shared_ = i; // 直接赋值为当前循环的计数值
         });
         handles.push(handle);
     }
     for handle in handles {
-        handle.join().unwrap();
-        // TODO: Print the value of the JobStatus.jobs_completed. Did you notice
-        // anything interesting in the output? Do you have to 'join' on all the
-        // handles?
-        println!("jobs completed {}", ???);
+        // 等待每个线程的结束
+        let _ = handle.join().unwrap();
+
+        // 获取当前共享状态并打印
+        let jobs_com = status.jobs_completed.lock().unwrap();
+        println!("jobs completed {}", *jobs_com);
     }
 }
